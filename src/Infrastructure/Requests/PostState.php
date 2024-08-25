c9fcf189c17ee444cba55f98dd8895fa684cb089
sakmarisin2 <120209335+sakmarisin2@users.noreply.github.com>
2024-08-25 21:12:31 +0400

    Update PostState.php

--- src/Infrastructure/Requests/PostState.php
+++ src/Infrastructure/Requests/PostState.php
@@ -4,7 +4,7 @@ use Domain\Interfaces\RequestState;
 use Domain\Core\BaseController;
 
 class PostState implements RequestState{
-    public function HandleRequest(BaseController $controller):void{
+    public function HandleRequest(BaseController $controller): void{
         $controller -> handlePost();
     } 
 }
