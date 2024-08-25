300b34b66fe395c5eaa06f68889167c7ca22a4c2
sakmarisin2 <120209335+sakmarisin2@users.noreply.github.com>
2024-08-25 21:14:20 +0400

    Update IntegerRule.php

--- src/Infrastructure/Validation/Rules/IntegerRule.php
+++ src/Infrastructure/Validation/Rules/IntegerRule.php
@@ -3,7 +3,7 @@ namespace Infrastructure\Validation\Rules;
 use Domain\Interfaces\ValidationRule;
 
 class IntegerRule implements ValidationRule {
-    public function validate($value): bool {
+    public function validate($value): bool{
         return filter_var($value, FILTER_VALIDATE_INT) !== false;
     }
 }
