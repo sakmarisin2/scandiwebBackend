34a5a87fb558c6b21cffec92dd8b00a1fee2ad2b
sakmarisin2 <120209335+sakmarisin2@users.noreply.github.com>
2024-08-25 21:11:26 +0400

    Update GatewayInterface.php

--- src/Domain/Interfaces/GatewayInterface.php
+++ src/Domain/Interfaces/GatewayInterface.php
@@ -4,7 +4,7 @@ namespace Domain\Interfaces;
 
 interface GatewayInterface{
     public function GetAll(): array;
-    public function CreateProduct(array $data):?int;
-    public function DeleteProducts(array $data):?string;
+    public function CreateProduct(array $data): ?int;
+    public function DeleteProducts(array $data): ?string;
 
 }
